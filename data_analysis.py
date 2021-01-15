cec85f1219474e617205419f7ecadf7e4fdca486
Faruk Colak <faruk_colak53@hotmail.com>
2021-01-15 22:35:36 +0300

    zıbap ıvjssss

--- data_analysis.py
+++ data_analysis.py
@@ -2,72 +2,85 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from matplotlib import rcParams
 
 
 def visualization(df_train):
+    print(df_train['SalePrice'].describe())  # We’re going to predict the SalePrice column ($ USD)
+
+    sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+    rcParams['figure.figsize'] = 14, 8
     sns.distplot(df_train['SalePrice'], fit=norm)
     (mu, sigma) = norm.fit(df_train['SalePrice'])
     plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)], loc='best')
     plt.title('Sale Prices')
     plt.xlabel('Sale Price')
     plt.ylabel('Probability')
-   # plt.close()
+    # plt.close()
     plt.show()
     # Most of the density lies between 100k and 250k, but there appears to be a lot of outliers on the pricier side.
 
     # -------------------------------- #
 
-    # Next, let’s have a look at the greater living area (square feet) against the sale price:
-
-    # Exploring the data - Sale Price vs GrLivArea
-    data = pd.concat([df_train['SalePrice'], df_train['GrLivArea']], axis=1)
-    data.plot.scatter(x='GrLivArea', y='SalePrice', ylim=(0, 800000), s=32)
-
-    # You might’ve expected that larger living area should mean a higher price.
-    # This chart shows you’re generally correct. But what are those 2–3 “cheap” houses offering huge living area?
-
+    # top 10 correlated features with the sale price:
     corr_matrix = df_train.corr()
-   # f, ax = plt.subplots(figsize=(12, 9))
     sns.heatmap(corr_matrix, vmax=.8, square=True)
-
-    # Let’s have a more general view on the top 10 correlated features with the sale price:
     k = 10  # number of variables for heat map
     cols = corr_matrix.nlargest(k, 'SalePrice')['SalePrice'].index
-   # f, ax = plt.subplots(figsize=(14, 10))
     sns.heatmap(df_train[cols].corr().values.T, cbar=True, annot=True, square=True, yticklabels=cols.values,
                 xticklabels=cols.values)
-    #plt.close()
+    # plt.close()
     plt.show()
 
     # Overall Quality vs Sale Price
     var = 'OverallQual'
     data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-   # f, ax = plt.subplots(figsize=(8, 6))
-    fig = sns.boxplot(x=var, y="SalePrice", data=data)
-    fig.axis(ymin=0, ymax=800000)
+    data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000), s=32)
     plt.show()
-
-    # What! People pay more for better quality? Nothing new here. Let's move on.
-
-    #                                -----
 
     # Living Area vs Sale Price
-    sns.jointplot(x=df_train['GrLivArea'], y=df_train['SalePrice'], kind='reg')
+    var = 'GrLivArea'
+    data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+    data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000), s=32)
     plt.show()
+
     # It makes sense that people would pay for the more living area.
     # What doesn't make sense is the two data points in the bottom-right of the plot.
-    # We need to take care of this! What we will do is remove these outliers manually.
 
     # Removing outliers manually (Two points in the bottom right)
     df_train = df_train.drop(df_train[(df_train['GrLivArea'] > 4000)
                                       & (df_train['SalePrice'] < 300000)].index).reset_index(drop=True)
 
-    # Living Area vs Sale Price
-    sns.jointplot(x=df_train['GrLivArea'], y=df_train['SalePrice'], kind='reg')
-    #plt.close()
+    # After removing outliers, Living Area vs Sale Price
+    var = 'GrLivArea'
+    data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+    data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000), s=32)
     plt.show()
 
-    # Bunları en etkili 10 parametre için yapabiliriz.
+    # GarageCars vs Sale Price
+    var = 'GarageCars'
+    data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+    data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000), s=32)
+    plt.show()
+
+    # GarageArea vs Sale Price
+    var = 'GarageArea'
+    data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+    data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000), s=32)
+    plt.show()
 
     # Up to this point, we were exploring the data
+
+    # Do we have missing data?
+
+    total = df_train.isnull().sum().sort_values(ascending=False)
+    percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending=False)
+    missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+
+    missing_data = missing_data[missing_data.Total > 0]
+
+    print(missing_data)
+
+    # Then, we will impute the missing data
+
     ################################################################
--- data_processing.py
+++ data_processing.py
@@ -4,117 +4,129 @@ import pandas as pd
 
 
 def processing(train_data, test_data):
-    labelencoder = LabelEncoder()
-    enc = OneHotEncoder(handle_unknown='ignore')
+    label_encoder = LabelEncoder()
+    one_hot_encoder = OneHotEncoder(handle_unknown='ignore')
+
+    # Remove outliers manually
 
     train_data = train_data.drop(train_data[(train_data['GrLivArea'] > 4000)
                                             & (train_data['SalePrice'] < 300000)].index).reset_index(drop=True)
     train_data = train_data.drop(train_data[(train_data['GarageCars'] > 3)
                                             & (train_data['SalePrice'] < 300000)].index).reset_index(drop=True)
-
     train_data = train_data.drop(train_data[(train_data['GarageArea'] > 1000)
                                             & (train_data['SalePrice'] < 300000)].index).reset_index(drop=True)
 
-    """
-    train_data['FireplaceQu'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['FireplaceQu'] = labelencoder.fit_transform(train_data['FireplaceQu'])
-    print(train_data['FireplaceQu'])"""
+    # Imputing missing data, and encoding some categorical data with label encoder and one hot encoder
 
     train_data['FireplaceQu'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['FireplaceQu'] = labelencoder.fit_transform(train_data['FireplaceQu'])
+    train_data['FireplaceQu'] = label_encoder.fit_transform(train_data['FireplaceQu'])
     test_data['FireplaceQu'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['FireplaceQu'] = labelencoder.fit_transform(test_data['FireplaceQu'])
+    test_data['FireplaceQu'] = label_encoder.fit_transform(test_data['FireplaceQu'])
 
     train_data['ExterQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['ExterQual'] = labelencoder.fit_transform(train_data['ExterQual'])
+    train_data['ExterQual'] = label_encoder.fit_transform(train_data['ExterQual'])
     test_data['ExterQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['ExterQual'] = labelencoder.fit_transform(test_data['ExterQual'])
+    test_data['ExterQual'] = label_encoder.fit_transform(test_data['ExterQual'])
 
     train_data['BsmtQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['BsmtQual'] = labelencoder.fit_transform(train_data['BsmtQual'])
+    train_data['BsmtQual'] = label_encoder.fit_transform(train_data['BsmtQual'])
     test_data['BsmtQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['BsmtQual'] = labelencoder.fit_transform(test_data['BsmtQual'])
+    test_data['BsmtQual'] = label_encoder.fit_transform(test_data['BsmtQual'])
 
     train_data['BsmtCond'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['BsmtCond'] = labelencoder.fit_transform(train_data['BsmtCond'])
+    train_data['BsmtCond'] = label_encoder.fit_transform(train_data['BsmtCond'])
     test_data['BsmtCond'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['BsmtCond'] = labelencoder.fit_transform(test_data['BsmtCond'])
+    test_data['BsmtCond'] = label_encoder.fit_transform(test_data['BsmtCond'])
 
     train_data['KitchenQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['KitchenQual'] = labelencoder.fit_transform(train_data['KitchenQual'])
+    train_data['KitchenQual'] = label_encoder.fit_transform(train_data['KitchenQual'])
     test_data['KitchenQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['KitchenQual'] = labelencoder.fit_transform(test_data['KitchenQual'])
+    test_data['KitchenQual'] = label_encoder.fit_transform(test_data['KitchenQual'])
 
     """train_data['BsmtExposure'].replace(to_replace=np.nan, value="Av", inplace=True)
-    train_data['BsmtExposure'] = labelencoder.fit_transform(train_data['BsmtExposure'])
+    train_data['BsmtExposure'] = label_encoder.fit_transform(train_data['BsmtExposure'])
     test_data['BsmtExposure'].replace(to_replace=np.nan, value="Av", inplace=True)
-    test_data['BsmtExposure'] = labelencoder.fit_transform(test_data['BsmtExposure'])
+    test_data['BsmtExposure'] = label_encoder.fit_transform(test_data['BsmtExposure'])
     
     train_data['BsmtFinType1'].replace(to_replace=np.nan, value="Rec", inplace=True)
-    train_data['BsmtFinType1'] = labelencoder.fit_transform(train_data['BsmtFinType1'])
+    train_data['BsmtFinType1'] = label_encoder.fit_transform(train_data['BsmtFinType1'])
     test_data['BsmtFinType1'].replace(to_replace=np.nan, value="Rec", inplace=True)
-    test_data['BsmtFinType1'] = labelencoder.fit_transform(test_data['BsmtFinType1'])
+    test_data['BsmtFinType1'] = label_encoder.fit_transform(test_data['BsmtFinType1'])
     
     train_data['BsmtFinType2'].replace(to_replace=np.nan, value="Rec", inplace=True)
-    train_data['BsmtFinType2'] = labelencoder.fit_transform(train_data['BsmtFinType2'])
+    train_data['BsmtFinType2'] = label_encoder.fit_transform(train_data['BsmtFinType2'])
     test_data['BsmtFinType2'].replace(to_replace=np.nan, value="Rec", inplace=True)
-    test_data['BsmtFinType2'] = labelencoder.fit_transform(test_data['BsmtFinType2'])
+    test_data['BsmtFinType2'] = label_encoder.fit_transform(test_data['BsmtFinType2'])
     
     train_data['HeatingQC'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['HeatingQC'] = labelencoder.fit_transform(train_data['HeatingQC'])
+    train_data['HeatingQC'] = label_encoder.fit_transform(train_data['HeatingQC'])
     test_data['HeatingQC'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['HeatingQC'] = labelencoder.fit_transform(test_data['HeatingQC'])"""
+    test_data['HeatingQC'] = label_encoder.fit_transform(test_data['HeatingQC'])"""
 
     """train_data['CentralAir'].replace(to_replace=np.nan, value=str(train_data['CentralAir'].mode()), inplace=True)
-    train_data['CentralAir'] = labelencoder.fit_transform(train_data['CentralAir'])
+    train_data['CentralAir'] = label_encoder.fit_transform(train_data['CentralAir'])
     test_data['CentralAir'].replace(to_replace=np.nan, value=str(train_data['CentralAir'].mode()), inplace=True)
-    test_data['CentralAir'] = labelencoder.fit_transform(test_data['CentralAir'])"""
+    test_data['CentralAir'] = label_encoder.fit_transform(test_data['CentralAir'])"""
 
     """train_data['Electrical'].replace(to_replace=np.nan, value="FuseA", inplace=True)
-    train_data['Electrical'] = labelencoder.fit_transform(train_data['Electrical'])
+    train_data['Electrical'] = label_encoder.fit_transform(train_data['Electrical'])
     test_data['Electrical'].replace(to_replace=np.nan, value="FuseA", inplace=True)
-    test_data['Electrical'] = labelencoder.fit_transform(test_data['Electrical'])"""
+    test_data['Electrical'] = label_encoder.fit_transform(test_data['Electrical'])"""
 
     """train_data['GarageQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['GarageQual'] = labelencoder.fit_transform(train_data['GarageQual'])
+    train_data['GarageQual'] = label_encoder.fit_transform(train_data['GarageQual'])
     test_data['GarageQual'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['GarageQual'] = labelencoder.fit_transform(test_data['GarageQual'])
+    test_data['GarageQual'] = label_encoder.fit_transform(test_data['GarageQual'])
     
     
     train_data['GarageCond'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['GarageCond'] = labelencoder.fit_transform(train_data['GarageCond'])
+    train_data['GarageCond'] = label_encoder.fit_transform(train_data['GarageCond'])
     test_data['GarageCond'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['GarageCond'] = labelencoder.fit_transform(test_data['GarageCond'])
+    test_data['GarageCond'] = label_encoder.fit_transform(test_data['GarageCond'])
     
     train_data['PoolQC'].replace(to_replace=np.nan, value="TA", inplace=True)
-    train_data['PoolQC'] = labelencoder.fit_transform(train_data['PoolQC'])
+    train_data['PoolQC'] = label_encoder.fit_transform(train_data['PoolQC'])
     test_data['PoolQC'].replace(to_replace=np.nan, value="TA", inplace=True)
-    test_data['PoolQC'] = labelencoder.fit_transform(test_data['PoolQC'])"""
+    test_data['PoolQC'] = label_encoder.fit_transform(test_data['PoolQC'])"""
 
     """train_data['Fence'].replace(to_replace=np.nan, value="GdWo", inplace=True)
-    train_data['Fence'] = labelencoder.fit_transform(train_data['Fence'])
+    train_data['Fence'] = label_encoder.fit_transform(train_data['Fence'])
     test_data['Fence'].replace(to_replace=np.nan, value="GdWo", inplace=True)
-    test_data['Fence'] = labelencoder.fit_transform(test_data['Fence'])"""
+    test_data['Fence'] = label_encoder.fit_transform(test_data['Fence'])"""
 
-    train_data['Neighborhood'] = pd.DataFrame(enc.fit_transform(train_data[['Neighborhood']]).toarray())
-    test_data['Neighborhood'] = pd.DataFrame(enc.fit_transform(test_data[['Neighborhood']]).toarray())
+    # train_data['Neighborhood'].replace(to_replace=np.nan, value="None", inplace=True)
+    train_data['Neighborhood'] = pd.DataFrame(one_hot_encoder.fit_transform(train_data[['Neighborhood']]).toarray())
+    # test_data['Neighborhood'].replace(to_replace=np.nan, value="None", inplace=True)
+    test_data['Neighborhood'] = pd.DataFrame(one_hot_encoder.fit_transform(test_data[['Neighborhood']]).toarray())
 
-    train_data['Heating'] = pd.DataFrame(enc.fit_transform(train_data[['Heating']]).toarray())
-    test_data['Heating'] = pd.DataFrame(enc.fit_transform(test_data[['Heating']]).toarray())
+    # train_data['Heating'].replace(to_replace=np.nan, value="None", inplace=True)
+    train_data['Heating'] = pd.DataFrame(one_hot_encoder.fit_transform(train_data[['Heating']]).toarray())
+    # test_data['Heating'].replace(to_replace=np.nan, value="None", inplace=True)
+    test_data['Heating'] = pd.DataFrame(one_hot_encoder.fit_transform(test_data[['Heating']]).toarray())
 
-    train_data['SaleType'].replace(to_replace=np.nan, value="None", inplace=True)
-    train_data['SaleType'] = pd.DataFrame(enc.fit_transform(train_data[['SaleType']]).toarray())
-    test_data['SaleType'].replace(to_replace=np.nan, value="None", inplace=True)
-    test_data['SaleType'] = pd.DataFrame(enc.fit_transform(test_data[['SaleType']]).toarray())
+    # train_data['SaleType'].replace(to_replace=np.nan, value="None", inplace=True)
+    train_data['SaleType'] = pd.DataFrame(one_hot_encoder.fit_transform(train_data[['SaleType']]).toarray())
+    # test_data['SaleType'].replace(to_replace=np.nan, value="None", inplace=True)
+    test_data['SaleType'] = pd.DataFrame(one_hot_encoder.fit_transform(test_data[['SaleType']]).toarray())
 
-    train_data['SaleCondition'] = pd.DataFrame(enc.fit_transform(train_data[['SaleCondition']]).toarray())
-    test_data['SaleCondition'] = pd.DataFrame(enc.fit_transform(test_data[['SaleCondition']]).toarray())
+    # train_data['SaleCondition'].replace(to_replace=np.nan, value="None", inplace=True)
+    train_data['SaleCondition'] = pd.DataFrame(one_hot_encoder.fit_transform(train_data[['SaleCondition']]).toarray())
+    # test_data['SaleCondition'].replace(to_replace=np.nan, value="None", inplace=True)
+    test_data['SaleCondition'] = pd.DataFrame(one_hot_encoder.fit_transform(test_data[['SaleCondition']]).toarray())
+
+    # Now, we select the numerical data from dataset
+    # These numerical data include the data that we encoded (categorical of old)
 
     num_train = train_data.select_dtypes(include=[np.number])
     num_test = test_data.select_dtypes(include=[np.number])
+
+    # Now, we impute the data for numerical ones
+
     num_train['LotFrontage'].replace(to_replace=np.nan, value=int(num_train['LotFrontage'].mean()), inplace=True)
     num_train['GarageYrBlt'].replace(to_replace=np.nan, value=int(num_train['GarageYrBlt'].mean()), inplace=True)
     num_train['MasVnrArea'].replace(to_replace=np.nan, value=0, inplace=True)
+
+    # Burada aynılarını neden train için yapmadık?
 
     num_test['LotFrontage'].replace(to_replace=np.nan, value=int(num_test['LotFrontage'].mean()), inplace=True)
     num_test['GarageYrBlt'].replace(to_replace=np.nan, value=int(num_test['GarageYrBlt'].mean()), inplace=True)
